7d7414d566ca43d38d6d0d81f120e01783609e60
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-10-17 13:00:38 +0800

    資料類型_藍圖_物件設置

---
+++ 2048_Hroizontal_20210919/Assets/Scripts/Car.cs
@@ -0,0 +1,22 @@
+//註解:說明文字,不備執行@#
+//功能 2021.10.17
+//開發者 BOBO 2021.10.17
+using UnityEngine;//引用 Uunity Engine API
+
+public class Car : MonoBehaviour    
+//class類別(藍圖)名稱要跟檔名相同,大小寫也要相同
+{
+    //認識欄位 Field ,儲存資料
+    //語法:
+    //資料類型 欄位名稱 指定 預設值 結束符號
+    //常用四大資料類型
+    //整數     沒有小數點的正負整數 int     預設值0
+    //浮點數   有小數點得正負整數   float   預設值0
+    //字串     文字資訊   string          預設值空
+    //布林值   是與否     true/false  bool    預設值false
+    int cc = 2000;
+    float weight = 3.5f;
+    string brand = "賓士";
+    bool hasSkyWindow = true;
+
+}
