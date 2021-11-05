ce7138258c0e07cde57d0c96972bec2dc901c88f
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-05 14:22:34 +0800

    屬性：屬性完整寫法，Get 內包含程式處理以及關鍵字 return 傳回

--- 2048_Hroizontal_20210919/Assets/Scripts/LearnProperty.cs
+++ 2048_Hroizontal_20210919/Assets/Scripts/LearnProperty.cs
@@ -9,13 +9,25 @@ public class    LearnProperty : MonoBehaviour
     //屬性    Property
     //語法
     //修飾詞 資料類型 屬性名稱{ 存取子 }
+    //自動實作
     public int passwordProperty { get; set; }
-    //get 取得
-    //set 設定
+   
     
     public int myPasswordProperty { get =>999; }
+    //get 取得
+    //set 設定
     // 唯讀屬性 只能取得
     //=> 黏巴達 Lambda c# 6.0版本
+
+    public string nameCharacter
+    {
+        get 
+        {
+            print("我在屬性 name Character 裡面~");
+            return "Bobo";//return傳回
+        }
+    }
+
 
     //開始事件:播放時執行一次
     private void Start()
@@ -31,5 +43,7 @@ public class    LearnProperty : MonoBehaviour
 
         ///myPasswordProperty = 999;//不能設定 唯讀屬性(只有get)
         print("我的屬性密碼 :" + myPasswordProperty);
+
+        print(nameCharacter);
     }
 }
