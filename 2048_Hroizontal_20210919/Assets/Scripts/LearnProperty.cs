ad58c6724bc6595a73fbe8fb3ba8a63298221121
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-05 14:16:31 +0800

    04_屬性：唯讀屬性以及黏巴達 Lambda 符號介紹與應用

--- 2048_Hroizontal_20210919/Assets/Scripts/LearnProperty.cs
+++ 2048_Hroizontal_20210919/Assets/Scripts/LearnProperty.cs
@@ -10,6 +10,12 @@ public class    LearnProperty : MonoBehaviour
     //語法
     //修飾詞 資料類型 屬性名稱{ 存取子 }
     public int passwordProperty { get; set; }
+    //get 取得
+    //set 設定
+    
+    public int myPasswordProperty { get =>999; }
+    // 唯讀屬性 只能取得
+    //=> 黏巴達 Lambda c# 6.0版本
 
     //開始事件:播放時執行一次
     private void Start()
@@ -22,5 +28,8 @@ public class    LearnProperty : MonoBehaviour
         //語法 :
         //屬性名稱
         print("屬性密碼 : " + passwordProperty);
+
+        ///myPasswordProperty = 999;//不能設定 唯讀屬性(只有get)
+        print("我的屬性密碼 :" + myPasswordProperty);
     }
 }
