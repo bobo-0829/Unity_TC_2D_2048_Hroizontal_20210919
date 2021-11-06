df2996b6ce5fd38a1a63c24781400b08dd703162
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-06 18:07:14 +0800

    判斷式 else 語法與練習

--- 2048_Hroizontal_20210919/Assets/Scripts/Learnif.cs
+++ 2048_Hroizontal_20210919/Assets/Scripts/Learnif.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class Learnif : MonoBehaviour
 {
+    public bool openDoor;
+
     private void Start()
     {
         // 判斷式 if
         //語法
         //如果(布林值) { 當布林值 等於 True 會執行 程式內容 }
+        //否則 { 當布林值 等於 false 會執行 程式內容 }
         if (true)
         {
             print("布林值 true");
         }
         if (false)
         {
-            print("布林值 true");
+            print("布林值 false");
+        }
+
+        //openDoor == true 與 openDoor
+        if (openDoor)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
         }
     }
 }
