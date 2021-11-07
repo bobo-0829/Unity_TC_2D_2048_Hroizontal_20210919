528e15aa92c7fce010e22847477c28efe70da072
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-07 16:50:23 +0800

    認識非靜態屬性

--- 2048_Hroizontal_20210919/Assets/Scripts/LearnPropertyNonStatic.cs
+++ 2048_Hroizontal_20210919/Assets/Scripts/LearnPropertyNonStatic.cs
@@ -11,6 +11,7 @@ public class LearnPropertyNonStatic : MonoBehaviour
     //2. 欄位資料與類別相同
     //3. 欄位要儲存資料
     public Camera cam;
+    public Transform tra;
 
     private void Start()
     {
@@ -21,6 +22,11 @@ public class LearnPropertyNonStatic : MonoBehaviour
         //取得非靜態屬性語法 :
         //欄位名稱.非靜態屬性名稱
         print("攝影機深度 :" + cam.depth);
+
+        //設定非靜態屬性語法 :
+        //欄位名稱.非靜態屬性名稱 指定 值 ;
+        tra.position = new Vector3(5, 0, 0);
+
 
     }
 }
