62f6bef136e416b90d10d6f172854f8b0b115127
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-07 11:18:53 +0800

    if及switch

--- 2048_Hroizontal_20210919/Assets/Scripts/Learnif.cs
+++ 2048_Hroizontal_20210919/Assets/Scripts/Learnif.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Learnif : MonoBehaviour
 {
     public bool openDoor;
+    public int score = 99;
 
+    // 開始
     private void Start()
     {
         // 判斷式 if
@@ -27,6 +29,28 @@ public class Learnif : MonoBehaviour
         else
         {
             print("關門");
+        }
+    }
+
+    // 跟新 : 一秒執行約六十次 60FPS
+    private void Update()
+    {
+        //如果分數 >=60 分 及格
+        if (score >= 60)
+        {
+            print("及格");
+        }
+        //如果分數 >= 40分 補考
+        //語法 : else if (布林值) { 布林值 為 true 時執行 }
+        //else if 放在 if 下方與 else 上方，可無限個
+        else if(score >= 40)
+        {
+            print("補考");
+        }
+        // 如果分數 < 40 分 死當
+        else
+        {
+            print("死當");
         }
     }
 }
