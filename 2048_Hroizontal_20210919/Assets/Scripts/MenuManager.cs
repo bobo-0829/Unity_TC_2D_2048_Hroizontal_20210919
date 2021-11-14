9db85e7e50a7ee53f93cdd1520102d3a2b045c14
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-14 16:59:43 +0800

    unity2021_11_14怪物設定

--- 2048_Hroizontal_20210919/Assets/Scripts/MenuManager.cs
+++ 2048_Hroizontal_20210919/Assets/Scripts/MenuManager.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.Audio;// 引用 音效 命名空間
 using UnityEngine.SceneManagement;//引用 場景管理 命名空間
+
 /// <summary>
 /// 開始畫面選單管理器
 /// 開始遊戲 設定 離開遊戲
 /// </summary>
-/// 繼承類別就可以存取其成員
+/// 繼承類別就可以存取其成員 :欄位、屬性、方法
 public class MenuManager : MonoBehaviour
 {
     // unity 按鈕與程式溝通
@@ -25,7 +26,7 @@ public class MenuManager : MonoBehaviour
         // 延遲 delay 秒後呼叫 方法
         Invoke("DelayStartGame", delay);
     }
-    public void DelayStartGame()
+    private void DelayStartGame()
     {
         // 場景管理.載入場景(場景名稱)
         SceneManager.LoadScene("遊戲場景");//載入遊戲場景
@@ -41,24 +42,32 @@ public class MenuManager : MonoBehaviour
         mixer.SetFloat("音量BGM", volume);
 
     }
-    ///<summary>
-    /// 設定遊戲
-    /// </summary>
     public void SettingGameSFX(float volume)
     {
         //print("SFX 滑桿音量" + volume);
         mixer.SetFloat("音效SFX", volume);
 
     }
+    public void SettingGame(float delay)
+    {
+        Invoke("DelaySettingGame", delay);
+    }
+
+
     ///<summary>
     /// 離開遊戲
     /// </summary>
-    public void QuitGame()
+    public void QuitGame(float delay)
+    {
+        Invoke("DelayQuitGame", delay);
+    }
+    private void DalayQuitGame()
     {
        // 應用程式.離開();
        // Quit 不會再 Editor 執行，發布執行檔 手機、PC
         Application.Quit();
 
         print("離開遊戲~");
+
     }
 }
