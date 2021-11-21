6be284b3771304b3457f1a17123011ec28673dfe
bobo-0829 <69894585+bobo-0829@users.noreply.github.com>
2021-11-21 11:48:03 +0800

    二維陣列+2048

---
+++ 2048_Hroizontal_20210919/Assets/Scripts/Learn2DArray.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/// <summary>
+///  認識二維陣列
+/// </summary>
+public class Learn2DArray : MonoBehaviour
+{
+    //一維陣列
+    public int[] numbers = { 1, 7, 9, 10, 50 };
+    //二維陣列 : 不會顯示 = 裝套件 Odin 自己寫
+    public int[,] blocks = { { 2, 4 }, { 6, 8 } };
+
+    public string[,] objects = new string[4, 6];
+
+    public int[,] scores = { { 70,90},{ 77,30},{ 10,20} };
+
+    private void Start()
+    {
+        //一維陣列存取
+        numbers[4] = 99;
+        print("一維陣列第五筆資料 :" + numbers[4]);
+
+        //二維陣列存取
+        print("二維陣列第二列第一欄 1 ,0 :" + blocks[1, 0]);
+        blocks[1, 0] = 7;
+        print("二維陣列第二列第一欄 1 ,0 :" + blocks[1, 0]);
+
+        print("二維陣列第一維度長度:" + scores.GetLongLength(0));
+        print("二維陣列第一維度長度:" + scores.GetLongLength(1));
+
+        string result = "";
+
+        for (int i = 0; i < scores.GetLongLength(0); i++)
+        {
+            for (int j = 0; j < scores.GetLongLength(1); j++)
+            {
+                result += scores[i, j] + "|";
+            }
+            result += "\n";
+        }
+        print(result);
+    }
+}
